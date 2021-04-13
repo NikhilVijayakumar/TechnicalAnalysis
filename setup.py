d1aa8810c30fc8a6345d1d64f6f569d5cf957b5f
Nikhil Vijayakumar <NikhilVijayakumar@users.noreply.github.com>
2021-04-13 12:19:54 +0530

    init

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='Trading',
+      version='1.0',
+      install_requires=[
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'cufflinks',
+        'seaborn',
+        'pandas_datareader',
+        'python-dateutil',
+    ]
+      )
